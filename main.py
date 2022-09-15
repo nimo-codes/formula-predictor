3b1a3fd94606bcff634fc12adec22bce520839e6
Naman nanda <64105119+nimo-codes@users.noreply.github.com>
2022-09-15 13:09:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import tensorflow
+from tensorflow import keras
+import numpy as np
+
+model = keras.Sequential([keras.layers.Dense(units=1,input_shape=[1])])
+model.compile(optimizer='sgd',loss='mean_squared_error')
+xs=np.array([-1.0,0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0],dtype=float)
+ys=np.array([-3.0,-1.0,1.0,3.0,5.0,7.0,9.0,11.0,13.0,15.0,17.0,19.0,21.0],dtype=float)
+model.fit(xs,ys,epochs=1000)
+
+print(model.predict([40.0]))
